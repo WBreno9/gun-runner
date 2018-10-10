8510e51d29c664ecedb3a6e522955adeded6e7dd
William Breno <williambreno@mat.ci.ufpb.br>
2018-10-09 22:27:47 -0300

    Added inc.h

---
+++ include/inc.h
@@ -0,0 +1,21 @@
+#ifndef INC_H
+#define INC_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <random>
+#include <cmath>
+#include <cstdlib>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/vector_angle.hpp>
+
+#endif
